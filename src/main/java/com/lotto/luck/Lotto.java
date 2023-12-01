a8cab3da072810c081fd19cc54ca1ecb7efb4abe
Vladimir Goncharenko <df.201640@yahoo.com>
2023-12-01 13:50:45 +0200

    2

--- src/main/java/com/lotto/luck/Lotto.java
+++ src/main/java/com/lotto/luck/Lotto.java
@@ -1,6 +1,8 @@
 package com.lotto.luck;
 
 import com.lotto.luck.model.Client;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import java.util.*;
 
 public class Lotto {
@@ -26,33 +28,31 @@ public class Lotto {
     }
 
     public List<Client> clientListGen(int numberClient) {
-        List<Client> clients = new ArrayList<>();
-        for (int i = 0; i < numberClient; i++) {
-            Client client = new Client();
-            client.setNumber(countClient);
-            List<Integer> generatedNumbers = lotteryLottoGen();
-            client.setList(generatedNumbers);
-            clients.add(client);
-            countClient++;
-        }
-        return clients;
+        return Stream
+                .iterate(countClient, i -> ++i)
+                .limit(numberClient)
+                .map(i -> {
+                            Client client = new Client();
+                            client.setNumber(i);
+                            client.setList(lotteryLottoGen());
+                            return client;
+                        }
+                ).collect(Collectors.toList());
     }
 
     public List<Integer> lotteryLottoGen() {
-        TreeSet<Integer> lotteryNumbers = new TreeSet<>((a, b) -> Integer.compare(b, a));
-
-        while (lotteryNumbers.size() < 6) {
-            int randomNumber = getRandomNumberInRange(1, 37);
-            lotteryNumbers.add(randomNumber);
-            addStatisticSixNumbers(randomNumber);
-        }
-
-        List<Integer> fullNumberList = new ArrayList<>(lotteryNumbers);
+        Set<Integer> lotteryNumbers = Stream
+                .generate(() -> getRandomNumberInRange(1, 37))
+                .distinct()
+                .limit(6)
+                .collect(Collectors.toCollection(TreeSet::new));
 
         int strongNumber = getRandomNumberInRange(1, 7);
-        fullNumberList.add(strongNumber);
-        addStatisticStrongNumber(strongNumber);
-        return fullNumberList;
+
+        return Stream.concat(lotteryNumbers.stream(), Stream.of(strongNumber))
+                .peek(this::addStatisticSixNumbers)
+                .peek(this::addStatisticStrongNumber)
+                .collect(Collectors.toList());
     }
 
     private int getRandomNumberInRange(int from, int to) {
@@ -62,56 +62,43 @@ public class Lotto {
 
     private void addStatisticSixNumbers(Integer generatedNumber) {
         String key = generatedNumber.toString();
-        if (statisticSixNumbers.containsKey(key)) {
-            Integer integer = statisticSixNumbers.get(key);
-            integer++;
-            statisticSixNumbers.put(key, integer);
-        } else {
-            statisticSixNumbers.put(key, 1);
-        }
+        statisticSixNumbers.put(key, statisticSixNumbers.containsKey(key) ? statisticSixNumbers.get(key) + 1 : 1);
     }
 
     private void addStatisticStrongNumber(Integer generatedNumber) {
         String key = generatedNumber.toString();
-        if (statisticStrongNumbers.containsKey(key)) {
-            Integer integer = statisticStrongNumbers.get(key);
-            integer++;
-            statisticStrongNumbers.put(key, integer);
-        } else {
-            statisticStrongNumbers.put(key, 1);
-        }
+        statisticStrongNumbers.put(key, statisticStrongNumbers.containsKey(key) ? statisticStrongNumbers.get(key) + 1 : 1);
     }
 
     public Map<String, List<Client>> calculateStatistics(List<Integer> lottery, List<Client> clients) {
-
-        clients.forEach(
-                client -> {
-                    boolean strongMatches = false;
-
-                    List<Integer> list = client.getList();
-
-                    if (lottery.get(6).equals(list.get(6))) {
-                        strongMatches = true;
-                    }
-
-                    int totalMatches = countMatches(lottery.subList(0, 6), list.subList(0, 6));
-
-                    if (totalMatches >= 3) {
-                        String groupKey = totalMatches + (strongMatches ? "+Strong" : "");
-                        List<Client> matchingClients = map.get(groupKey);
-                        matchingClients.add(client);
-                    }
-                }
-        );
+        clients
+                .forEach(client -> {
+                            boolean strongMatches = lottery.get(6).equals(client.getList().get(6));
+                            int totalMatches = countMatches(lottery.subList(0, 6), client.getList().subList(0, 6));
+                            if (totalMatches >= 3) {
+                                String groupKey = totalMatches + (strongMatches ? "+Strong" : "");
+                                map.computeIfAbsent(groupKey, k -> new ArrayList<>()).add(client);
+                            }
+                        }
+                );
         return map;
     }
 
-    public void printLotteryStatistic() {
-        for (String key : map.keySet()) {
-            List<Client> clients = map.get(key);
-            System.out.printf("%-8s: %d%n", key, clients.size());
-        }
+    private void printStatistics(Map<String, Integer> statistics, int limit) {
+        statistics
+                .entrySet()
+                .stream()
+                .sorted(Map.Entry.<String, Integer>comparingByValue().reversed())
+                .limit(limit)
+                .forEach(entry -> System.out.println(entry.getKey() + ": " + entry.getValue()));
+    }
 
+    private int countMatches(List<Integer> lottery, List<Integer> clientNumbers) {
+        return (int) lottery.stream().filter(clientNumbers::contains).count();
+    }
+
+    public void printLotteryStatistic() {
+        map.forEach((key, clients) -> System.out.printf("%-8s: %d%n", key, clients.size()));
     }
 
     public void printStatisticsLottoNumbers() {
@@ -124,30 +111,6 @@ public class Lotto {
 
         System.out.println("Statistic for strong numbers:");
         printStatistics(statisticStrongNumbers, 7);
-    }
-
-    private void printStatistics(Map<String, Integer> statistics, int limit) {
-        List<Map.Entry<String, Integer>> entries = new ArrayList<>(statistics.entrySet());
-        entries.sort((entry1, entry2) -> entry2.getValue().compareTo(entry1.getValue()));
-
-        int count = 0;
-        for (Map.Entry<String, Integer> entry : entries) {
-            System.out.println(entry.getKey() + ": " + entry.getValue());
-            count++;
-            if (count >= limit) {
-                break;
-            }
-        }
-    }
-
-    private int countMatches(List<Integer> lottery, List<Integer> clientNumbers) {
-        int matchCount = 0;
-
-        for (Integer number : lottery) {
-            if (clientNumbers.contains(number)) {
-                matchCount++;
-            }
-        }
-        return matchCount;
+        System.out.println();
     }
 }
